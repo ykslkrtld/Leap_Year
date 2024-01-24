cbb18aac1a408887d89a0ef1cc05c5973a111b9a
yksl <yukselkurtuldu9@gmail.com>
2024-01-24 16:31:10 +0300

    first commit

---
+++ leap_year.js
@@ -0,0 +1,9 @@
+// Leap Year
+
+const year = Number(prompt("Enter a year"))
+
+if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
+    alert(`${year} is a leap year`)
+} else{
+    alert(`${year} is not a leap year`)
+}
